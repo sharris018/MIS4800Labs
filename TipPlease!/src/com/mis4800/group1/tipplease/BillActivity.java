6c2a1b3a6b754ae4c2f085a202d2d4836d059a54
Shonelle Harris <harris.499@wright.edu>
2016-12-02 11:13:39 -0500

    Updated tip app

--- TipPlease!/src/com/mis4800/group1/tipplease/BillActivity.java
+++ TipPlease!/src/com/mis4800/group1/tipplease/BillActivity.java
@@ -3,10 +3,35 @@
  */
 package com.mis4800.group1.tipplease;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 /**
  * @author shone
  *
  */
-public class BillActivity {
+public class BillActivity extends Activity {
+	
+	Button btnEnter2; 
+	EditText txtItem, txtPrice, txtTotal, txtTip; 
+	TextView tvTip, tvTotal;
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_singlebill);
+
+	} 
+	
+	
+	
+	
+	
 
 }
--- TipPlease!/src/com/mis4800/group1/tipplease/MainActivity.java
+++ TipPlease!/src/com/mis4800/group1/tipplease/MainActivity.java
@@ -13,26 +13,19 @@ import android.widget.Spinner;
 
 public class MainActivity extends Activity implements OnClickListener {
 
-	EditText txtItem, txtTotal, txtTip, txtPrice, txtParties;
-	Button btn_splitback, btnEnter, btnEnter2, btnMainMenu;
+	Button btnEnter;
 	Spinner billoption; 
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		// Grab the text fields 
-		txtItem = (EditText) findViewById(R.id.txtItem);
-		txtTotal = (EditText) findViewById(R.id.txtTotal);
-		txtTip = (EditText) findViewById(R.id.txtTip);
-		txtPrice = (EditText) findViewById(R.id.txtPrice);
-		txtParties = (EditText) findViewById(R.id.txtParties);
+		
 		
 		// Grab the buttons 
 		
-		btn_splitback = (Button) findViewById(R.id.btn_splitback);
 		btnEnter = (Button) findViewById(R.id.btnEnter);
-		btnEnter2 = (Button) findViewById(R.id.btnMainMenu);
+		
 		
 		// Grab spinner button 
 		
@@ -67,11 +60,24 @@ public class MainActivity extends Activity implements OnClickListener {
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
+		
 		if (v.getId() == R.id.btnEnter) {
-		} else if (v.getId() == R.id.btnEnter2) {
-			Intent mainIntent = new Intent(this, PartiesActivity.class);
+			
+			 
+	        switch (billoption.getSelectedItemPosition()) {
+            case 0:
+                // Select the single bill option 
+            	
+                break;
+            case 1:
+                // Select the multiple bill option, this option splits the check 
+            	
+                break;
+	        }
+			
+			Intent mainIntent = new Intent(this, BillActivity.class);
 			startActivity(mainIntent);
-		}
-	}
+		
+	}}
 
 }
